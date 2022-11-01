2cbb9972d3acecb3da3d9df5c2244344bf94a87c
gergof2 <71332043+gergof2@users.noreply.github.com>
2022-11-01 11:49:09 +0100

    App szerkesztése, autentikáció meghívása

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import UserContext from './auth/AccountContext';
+
+import React from 'react';
+
 import {StyledContainer} from './components/Styles/Styles';
+
 import Views from './pages/Views';
+
+function App() {
+  return (
     <UserContext>
       <StyledContainer>
         <Views/>
       </StyledContainer>    
     </UserContext>
+  );
+}
+
+export default App;
