a1fcfaef42ac377e1eb0fb10e097d1d69c58da1b
gergof2 <71332043+gergof2@users.noreply.github.com>
2022-11-01 11:50:41 +0100

    Home és Dashboard beimportálása

--- client/src/pages/Views.js
+++ client/src/pages/Views.js
@@ -2,19 +2,26 @@ import { AccountContext } from '../auth/AccountContext';
 import PrivateRoutes from '../auth/PrivateRoutes';
 import React,{ useContext } from 'react';
 
+import Home from './Home';
 import Login from './Login';
+import Dashboard from './Dashboard';
 import {
     BrowserRouter as Router,
     Routes,
     Route
   } from 'react-router-dom';
+  const Views = () => {
+
     const {user} = useContext(AccountContext)
     return (
         <Router>
           <Routes>
+              <Route path="/" element={user.loggedIn === true ? (<Dashboard />) :(<Home />)}/>
               <Route path="/login" element={user.loggedIn === true ? (<Dashboard />) :(<Login />) }/>               
               <Route element={<PrivateRoutes/>}>
+                  <Route path="/dashboard" element={<Dashboard />} />
               </Route>
+              <Route path="*" element={user.loggedIn === true ?  (<Dashboard />) :(<Home />)}/>
           </Routes>
         </Router>
     );
