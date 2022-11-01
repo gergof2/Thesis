cb88444e9dd6e31cbaf323863710c8d1b66fec90
gergof2 <71332043+gergof2@users.noreply.github.com>
2022-11-01 11:45:06 +0100

    Oldalak létrehozása

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import UserContext from './auth/AccountContext';
+import {StyledContainer} from './components/Styles/Styles';
+import Views from './pages/Views';
+    <UserContext>
+      <StyledContainer>
+        <Views/>
+      </StyledContainer>    
+    </UserContext>
