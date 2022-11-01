e9ab8307aa6ba526ee23f54521482d7fe5d106b8
gergof2 <71332043+gergof2@users.noreply.github.com>
2022-11-01 11:41:02 +0100

    Kész tesztek feltöltése(felhasználó, pontszám, szavak)

--- server/api/Words.js
+++ server/api/Words.js
@@ -26,3 +26,29 @@ router.post('/getwords', async (req, res) => {
     else return res.json({message: "Ismeretlen felhasználó!"});
 });
 
+router.post('/posttest', async (req, res) => {   
+    if(req.session.user && req.session.user.email){    
+        let data = [];   
+        let p = req.body.engwords;
+        let h = req.body.hunwords;
+        const result = p.toString().replace('\\r\\n', '');
+        const result2 = h.toString().replace('\n', '');
+        var records = [req.session.user.id, result, result2, req.body.score, req.body.mode];
+        function post_Test(callback){
+            sql.query("INSERT INTO tests(user_id, engwords, hunwords, score, mode) values (?)", [records], function(err, results){
+                if(err) return err;
+                data = results[0]
+                return callback(results[0]);
+            })
+        }
+        post_Test(async function(result){
+            data = result;
+            return res.json({data: data});
+        })
+    }
+    else return res.json({message: "Ismeretlen felhasználó!"});
+});
+
+
+
+module.exports = router;
